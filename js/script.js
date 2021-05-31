33ec7904686004a23b1b24e17b1e09e4037be04d
Reverson <reverson@pagprime.com.br>
2021-05-31 14:26:34 -0300

    valida button pronto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 const init = () => {
+  buttonSubmit = {
+    button: document.querySelector("#button"),
+    enable: function () {
+      this.button.classList.add("disabled");
+      document.getElementById("button").disabled = false;
+    },
+    disable: function () {
+     
+      this.button.classList.remove("disabled");
+      document.getElementById("button").disabled = true;
+    },
+  };
+
+  buttonSubmit.disable();
+
   function searchUrl() {
     var currentLocation = window.location.pathname;
     const url = `./data.json`;
@@ -10,11 +25,6 @@ const init = () => {
 
   searchUrl();
 
-  const inputNome = document.querySelector('input[name="name"]');
-  const inputEmail = document.querySelector('input[type="email"]');
-  const inputTelefone = document.querySelector('input[name="phone"]');
-  const inputCPF = document.querySelector('input[name="cpf"]');
-  const submitButton = document.getElementById("button");
 
   alert("init carregada");
 
@@ -45,9 +55,8 @@ const init = () => {
   };
 
   const validateOnly = (event) => {
-    const input = event.currentTarget;
-
-    if (input.value.length == 0) submitButton.removeAttribute("disabled");
+    const input = event.target;
+    return input.value.length > 3;
   };
 
   const errorHandler = () => {
@@ -56,18 +65,62 @@ const init = () => {
     submitButton.textContent = "Dados inválidos";
   };
 
-  if (submitButton) {
-    submitButton.addEventListener("click", (event) => {
-      
-      inputEmail.addEventListener("input", validateEmail);
-      inputCPF.addEventListener("input", validateLength);
-      inputTelefone.addEventListener("input", validateLength);
-      inputNome.addEventListener("input", validateOnly);
-      inputTelefone.addEventListener("input", validateOnly);
-      alert("teste");
-      //submitButton.textContent = "...loading"
+
+  document.querySelectorAll("input").forEach((listaInput) => {
+    listaInput.addEventListener("keyup", (event) => {
+      let input = event.target;
+      let response;
+      switch (input.name) {
+        case "name":
+          response = validateOnly(event);
+          console.log(response);
+          break;
+
+        case "mail":
+          response = validateOnly(event);
+          //inputEmail.addEventListener("input", validateEmail);
+          console.log(response);
+
+          break;
+
+        case "cpf":
+          response = validateOnly(event);
+          //inputCPF.addEventListener("input", validateLength);
+          console.log(response);
+
+          break;
+
+        case "phone":
+          response = validateOnly(event);
+          //inputTelefone.addEventListener("input", validateLength);
+          //inputTelefone.addEventListener("input", validateOnly);
+          console.log(response);
+
+          break;
+
+        default:
+          0;
+      }
+
+      if (response) {
+        input.setAttribute("valid", true);
+      } else {
+        input.setAttribute("valid", false);
+      }
+
+      checkButton();
     });
-  }
+  });
+
+  const checkButton = () => {
+    const totalValid = document.querySelectorAll('input[valid="true"]').length;
+
+    if (totalValid == document.querySelectorAll("input").length) {
+      buttonSubmit.enable();
+    } else {
+      buttonSubmit.disable();
+    }
+  };
 };
 
 window.onload = init;
